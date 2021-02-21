abce47c3daf597cc1e3648782a95f0e2b09fb9ff
princewill <princewillolaiya@gmail.com>
2021-02-21 08:06:39 +0100

    added project to admin page

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Documentation
+from .models import Documentation, Project
 # Register your models here.
 admin.site.register(Documentation)
+admin.site.register(Project)
